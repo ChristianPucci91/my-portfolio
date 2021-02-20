4637165a490967366e621830d7636046f9bc7275
ChristianPucci91 <pucci.christian91@gmail.com>
2021-02-20 02:26:59 +0100

    Add jQuery carousel on About page

--- src/app.js
+++ src/app.js
@@ -1,1 +1,71 @@
-console.log('hello');
+
+$(document).ready(function () {
+
+  $(".next").click(nextImg);
+  $(".prev").click(prevImg);
+
+  function nextImg() {
+
+    var imgActive = $("div.active");
+
+    imgActive.removeClass("active");
+
+    if (imgActive.hasClass("last")){
+
+      $("div.first").addClass("active");
+
+    } else {
+
+      imgActive.next("div").addClass("active");
+
+    }
+
+    var circleActive = $("i.active");
+    circleActive.removeClass("active");
+    if (circleActive.hasClass("last")) {
+      $("i.first").addClass("active");
+    }else {
+     circleActive.next("i").addClass("active");
+    }
+
+  }
+
+  function prevImg() {
+
+    var imgActive = $("div.active");
+
+    imgActive.removeClass("active");
+
+    if (imgActive.hasClass("first")){
+
+      $("div.last").addClass("active");
+
+    } else {
+
+      imgActive.prev("div").addClass("active");
+
+    }
+
+    var circleActive = $("i.active");
+    circleActive.removeClass("active");
+    if (circleActive.hasClass("first")) {
+      $("i.last").addClass("active");
+    }else {
+     circleActive.prev("i").addClass("active");
+    }
+
+  }
+
+  $("i.fa-circle").click(function () {
+
+    $(".nav > .active").removeClass("active");
+    $(this).addClass("active");
+    var clicked = $(this).index();
+    $("div.active").removeClass("active");
+    var imgSelect = $(".experiences > div");
+    var imgShow = "div:nth-child(" + (clicked + 1) + ")";
+    $(imgShow).addClass("active");
+
+  })
+
+});
